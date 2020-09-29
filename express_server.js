23991a900bad26ebc9626c5de33c55a5e8933840
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-29 17:02:22 +0000

    comments added for clarity, button creation and route for deleting urls

--- express_server.js
+++ express_server.js
@@ -14,7 +14,7 @@ const generateRandomString = () => Math.random().toString(36).substring(2,8);
 
 app.set("view engine", "ejs");
 
-//hardcoded urls to start with
+//hardcoded urls to start with and our database since we aren't saving to an external
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,10 +32,10 @@ app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   //saves to object urlDatabase
   urlDatabase[shortURL] = req.body["longURL"];
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);         // Redirects us to newly created short url
 });
 
-// to redirect the short url to the long url page
+// to redirect the short url hyper link to the long url page
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
@@ -70,6 +70,13 @@ app.get("/urls/:shortURL", (req, res) => {
     res.send("400 Bad Request").statusCode(400);
     }
 });
+
+//route for deleting url, then redirects to urls page
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
