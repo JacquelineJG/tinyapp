d241291000a000642047c72074d91d7ad44cc941
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-30 19:40:10 +0000

    added working user registration that checks for email already in database object

--- express_server.js
+++ express_server.js
@@ -8,9 +8,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
-
+// HELPER FUNCTIONS
 //used for generating unique shortURL
 const generateRandomString = () => Math.random().toString(36).substring(2,8);
+//used for checking existence of email in users object
+const emailVerify = (obj, email) => {
+  for (const user in obj) {
+    const thisUser = obj[user]
+    if (thisUser.email === email) {
+      return true
+    }
+  }
+  return null
+}
 
 // const newObj = {
 //   Apple: "red"
@@ -20,17 +30,31 @@ const generateRandomString = () => Math.random().toString(36).substring(2,8);
 // newObj["Banana"] = "yellow"
 
 
-
-
-
 //URL DATABASE OBJECT
 //hardcoded urls to start with and our database since we aren't saving to an external
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//BODY PARSER for post requests to make human readable data
-
+//USER DATABASE OBJECT
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+app.use( (req, res, next) => {
+  const user_id = req.cookies["user_id"]
+  const user = users[user_id];
+  req.user = user
+  next();
+})
 ////////////////GET/////////////////
 
 app.get("/", (req, res) => {
@@ -45,7 +69,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"] 
+    user: req.user
   };
   res.render("urls_new", templateVars);
   
@@ -62,20 +86,29 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"] 
+    user: req.user
   };
   res.render("urls_index", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    email: req.cookies["email"], 
+    password: req.cookies["password"],
+    user: req.user
+  };
+  res.render("register_page", templateVars);
 });
 
 ///urls represents the page and : declares a variable input in the page url bar and shortURL is the variable name of the parameter
 app.get("/urls/:shortURL", (req, res) => {
   // console log of req.params so I can remember what it does
-  //console.log(req.params)
+  // console.log(req.params)
   // this variable displays the longURL by reaching into the urlDatabase and uses the req.params.shortURL to get the value 
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    user: req.user
    };
   res.render("urls_show", templateVars);
   //if statement for if shortURL returns a falsey value
@@ -111,7 +144,7 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   //console.log(req.body);
-  res.cookie("username", req.body["username"])
+  res.cookie("user_id", userID)
     // ... any other vars
     
   res.redirect("/urls")
@@ -119,13 +152,29 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   //console.log(req.body);
-  res.clearCookie("username")
+  res.clearCookie("user_id", userID)
     // ... any other vars
     
   res.redirect("/urls")
 });
   
-
+app.post("/register", (req, res) => {
+  userID = generateRandomString();
+  const { email, password } = req.body
+  if (emailVerify(users, email)) {
+    res.send("400 Bad Request: Email already in use").statusCode(400);
+  } else {
+    let newUser = {
+      id: userID,
+      email, 
+      password
+    }
+  users[userID] = newUser;
+  res.cookie("user_id", userID)
+  console.log(users)
+  res.redirect("/urls")
+}
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
